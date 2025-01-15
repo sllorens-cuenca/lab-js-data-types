f5f05b533cfe901b1c918c2785add5f33a7e4d19
sllorens-cuenca <sllorens.cuenca@gmail.com>
2025-01-15 15:30:35 +0100

    Solved lab Sergio Llorens

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 
 
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,9 +23,15 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+function camelTail(str) {
+    let final = str.slice(0, str.length - 1) + str[str.length - 1].toUpperCase();
+    return final
+}
 
 // Print the cameLtaiL-formatted string
+
+const result = camelTail(part1) + camelTail(part2);
+console.log(result);
 
 
 
@@ -35,9 +42,17 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+let tipAmount;
 
+function calculateTip(price) {
+    tipAmount = price + ((price * 15) / 100);
+    return tipAmount;
+}
 
 // Print out the tipAmount
+
+console.log(calculateTip(billTotal));
+
 
 
 
@@ -47,9 +62,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor((Math.random() * 10) + 1);
 
 // Print the generated random number
+
+console.log(randomNumber);
 
 
 
@@ -62,15 +79,30 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
 
 const expression2 = a || b;
+// true
 
 const expression3 = !a && b;
+//false
 
 const expression4 = !(a && b);
+//true
 
 const expression5 = !a || !b;
+//true
 
 const expression6 = !(a || b);
+//false
 
 const expression7 = a && a;
+//true
+
+console.log(expression1);
+console.log(expression2);
+console.log(expression3);
+console.log(expression4);
+console.log(expression5);
+console.log(expression6);
+console.log(expression7);
